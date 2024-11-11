4eb7a24c2feab9f40709c5ffd938c1179d44158f
Harpreet919 <harpreetsinghnz2003@gmail.com>
2024-11-11 15:36:34 +1300

    add the new buttoni created login branch

--- CP ryzen/Form1.Designer.cs
+++ CP ryzen/Form1.Designer.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Drawing; // Required for color and image usage
 using System.Windows.Forms;
-//This is the Login group (Harpreet)
+//This is the Login group (Harpreet singh)
 namespace ShippingManagementSystem
 {
     partial class frmLogin
