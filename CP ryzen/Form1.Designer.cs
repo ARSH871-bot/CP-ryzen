54b67e23127fda17b7827d98cec97fc17c03da28
SamikaPerera <samika.ashen@gmail.com>
2024-11-13 12:42:54 +1300

    I have added some commemnts

--- CP ryzen/Form1.Designer.cs
+++ CP ryzen/Form1.Designer.cs
@@ -47,8 +47,8 @@ namespace ShippingManagementSystem
             // 
             // pictureBoxLogo
             // 
-            this.pictureBoxLogo.Image = Image.FromFile(@"C:\ARSHOP\CP ryzen\Images\Logo.jpg"); // Replace with actual path
-            this.pictureBoxLogo.Location = new System.Drawing.Point((this.ClientSize.Width - 120) / 2, 40); // Center the logo
+            this.pictureBoxLogo.Image = Image.FromFile(@"C:\ARSHOP\CP ryzen\Images\Logo.jpg"); 
+            this.pictureBoxLogo.Location = new System.Drawing.Point((this.ClientSize.Width - 120) / 2, 40); 
             this.pictureBoxLogo.Size = new System.Drawing.Size(120, 60);
             this.pictureBoxLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
 
