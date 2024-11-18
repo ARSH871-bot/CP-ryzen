b862c392ee4626afd4c5fb72aeb138d15fe0fe61
Harpreet919 <harpreetsinghnz2003@gmail.com>
2024-11-18 23:11:23 +1300

    fix the error

--- CP ryzen/frmTracking.Designer.cs
+++ CP ryzen/frmTracking.Designer.cs
@@ -193,12 +193,7 @@
             this.ResumeLayout(false);
             this.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pbTrackingIcon)).EndInit();
-            this.Name = "frmTracking";
-            this.Text = "Shipment Tracking";
-            this.Load += new System.EventHandler(this.frmTracking_Load);
-            this.ResumeLayout(false);
-            this.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pbTrackingIcon)).EndInit();
+
         }
     }
 }
